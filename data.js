71b8efaad0487201632a9a99cebd4c690e605779
michela parma <44635720+michelaparma@users.noreply.github.com>
2020-09-02 16:25:02 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,151 @@
+var data = [
+    {
+        "title":"Week 1",
+        "period":["01/09/2020","04/09/2020"],
+        "highlights":
+                    [
+                        "Preliminary research of bibliographic material and preliminary analysis of them."
+                        
+                    ],
+        "tuesday":[
+            {
+                "title":"Preliminary research",
+                "text":"Preliminary research of bibliographic documentation about the city of Rimini, in particular, in relation to aspects of seaside tourism and entertainment. "
+            },
+            {
+                "title":"Bibliographic consultation",
+                "text":"The following documents have been consulted: <br> - L. Santese (edited by), <i>Italy&Italy. Fotografie di Pasquale Bove</i>, Pianello Val Tidone, Cesura Publish, 2016. <br> - P. V. Tondelli, <i>Rimini</i>, Milano, Bompiani, 1985. <br> - P. V. Tondelli, <i>Rimini: il romanzo vent'anni dopo: 2985-2005</i>, F. Panzeri (edited by), Rimini, Guaraldi, 2005. <br> - M. Serafini (edited by), <i>Rimini underground: le avanguardie grafiche anni' 80</i>, Rimini, GuaraldiLAB, 2017. <br> - M. Serafini (edited by), <i>Foto-grafia: l'underground degli anni '80</i>, Rimini, GuaraldiLAB, 2018. <br> - M. Serafini (edited by), <i>Saluti da Rimini: l'immaginario riminese nelle cartoline</i>, Rimini, GuaraldiLAB, 2015. <br> - L. Brandina, S. Cicchetti (edited by), <i>La notte della Riviera '90</i>, Rimini, Maggioli, 1990. <br> - A. La Motta, <i>Cartoline per Rimini</i>, Rimini, Raffaelli, 2000. <br> - Rimini Turismo, <i>Rimini: Benvenuti a Rimini capitale dell'ospitalità</i>, Rimini, Rimini Turismo, 1997. "    
+            },
+            
+        ],
+        
+        "wednesday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "thursday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ], 
+        "friday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+    },
+
+    {
+        "title":"",
+        "period":["",""],
+        "highlights":
+                    [
+                        "",
+                        "",
+                        ""
+                    ],
+        "monday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""    
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "tuesday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "wednesday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "thursday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ], 
+        "friday":[
+            {
+                "title":"",
+                "text":""
+            },
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+    },
+
+    
+
+    {
+        "title":"",
+        "period":["",""],
+        "highlights":
+                    [
+                        "",
+                        ""
+                    ],
+        "achievements":[
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "brainstorming":[
+            {
+                "title":"",
+                "text":""
+            }
+        ],
+        "future":[
+            {
+                "title":"",
+                "text":""
+            }
+        ]
+    }
+
+
+
+]
