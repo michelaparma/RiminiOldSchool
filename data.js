f1a9ef531e37c4823e9513724d64d42b16278eed
michela parma <44635720+michelaparma@users.noreply.github.com>
2020-09-04 13:19:45 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,54 +1,41 @@
 var data = [
     {
         "title":"Week 1",
-        "period":["01/09/2020","04/09/2020"],
+        "period":["02/09/2020","04/09/2020"],
         "highlights":
                     [
                         "Preliminary research of bibliographic material and preliminary analysis of them."
                         
                     ],
-        "tuesday":[
+        "wednesday":[
+            {
+                "title":"Start of the curricular internship",
+                "text":"Brainstorming with the company to establish the project steps. The name of the project has been changed to <i>Rimini Old School</i>."
+            },
             {
                 "title":"Preliminary research",
                 "text":"Preliminary research of bibliographic documentation about the city of Rimini, in particular, in relation to aspects of seaside tourism and entertainment. "
             },
             {
                 "title":"Bibliographic consultation",
-                "text":"The following documents have been consulted: <br> - L. Santese (edited by), <i>Italy&Italy. Fotografie di Pasquale Bove</i>, Pianello Val Tidone, Cesura Publish, 2016. <br> - P. V. Tondelli, <i>Rimini</i>, Milano, Bompiani, 1985. <br> - P. V. Tondelli, <i>Rimini: il romanzo vent'anni dopo: 2985-2005</i>, F. Panzeri (edited by), Rimini, Guaraldi, 2005. <br> - M. Serafini (edited by), <i>Rimini underground: le avanguardie grafiche anni' 80</i>, Rimini, GuaraldiLAB, 2017. <br> - M. Serafini (edited by), <i>Foto-grafia: l'underground degli anni '80</i>, Rimini, GuaraldiLAB, 2018. <br> - M. Serafini (edited by), <i>Saluti da Rimini: l'immaginario riminese nelle cartoline</i>, Rimini, GuaraldiLAB, 2015. <br> - L. Brandina, S. Cicchetti (edited by), <i>La notte della Riviera '90</i>, Rimini, Maggioli, 1990. <br> - A. La Motta, <i>Cartoline per Rimini</i>, Rimini, Raffaelli, 2000. <br> - Rimini Turismo, <i>Rimini: Benvenuti a Rimini capitale dell'ospitalità</i>, Rimini, Rimini Turismo, 1997. "    
+                "text":"The following documents have been consulted: <br> - L. Santese (edited by), <i>Italy&Italy. Fotografie di Pasquale Bove</i>, Pianello Val Tidone, Cesura Publish, 2016. <br> - P. V. Tondelli, <i>Rimini</i>, Milano, Bompiani, 1985. <br> - P. V. Tondelli, <i>Rimini: il romanzo vent'anni dopo: 2985-2005</i>, F. Panzeri (edited by), Rimini, Guaraldi, 2005. <br> - M. Serafini (edited by), <i>Rimini underground: le avanguardie grafiche anni' 80</i>, Rimini, GuaraldiLAB, 2017. <br> - M. Serafini (edited by), <i>Foto-grafia: l'underground degli anni '80</i>, Rimini, GuaraldiLAB, 2018. <br> - M. Serafini (edited by), <i>Saluti da Rimini: l'immaginario riminese nelle cartoline</i>, Rimini, GuaraldiLAB, 2015.  "    
             },
             
         ],
         
-        "wednesday":[
-            {
-                "title":"Informative and photographic material",
-                "text":"I continued my research for information and photographs in the library."
-            },
-            {
-                "title":"Bibliographic consultation",
-                "text":"The following documents have been consulted: <br> - C. Cederna, <i>Casa nostra</i>, Milano, A. Mondadori, 1983. <br> - A. Bonomi, <i>Il distretto del piacere</i>, Torino, Bollati Boringhieri, 2000. <br> - F. Farina, <i>Una costa lunga due secoli: storie e immagini della riviera di Rimini</i>, Rimini, Fondazione Cassa di Risparmio, 2003. <br> - F. Farina, <i>1843-1993: centocinquant'anni di vita balneare</i>, Rimini, Societa Gas, 1993. <br> - O. Maroni (edited by), <i>Davide Minghini fotografo in Rimini: immagini dall'archivio</i>, Bologna, Compositori, 2003. <br> - V. Bessone, <i>La divina notte: viaggio quasi dantesco fra i gironi delle discoteche riminesi</i>, Rimini, Guaraldi, 1994. <br> - P. Pacoda, <i>Riviera Club Culture: la scena dance nella metropoli balneare d'Europa</i>, Cerasolo Ausa di Coriano, NdA Press, 2012. <br> - M. Zardini, <i>Nomare: nascita e sviluppo della metropoliriviera</i>, Bologna, Editrice Compositori, 2006. <br> - C. Branzaglia (edited by), <i>NightWave: atti dei convegni</i>, Genova, Costa & Nolan, 1998. <br> - P. V. Tondelli, <i>Un weekend postmoderno: cronache dagli anni Ottanta</i>, Milano, Bompiani, 1990."
-            }
-        ],
         "thursday":[
             {
-                "title":"",
-                "text":""
-            },
-            {
-                "title":"",
-                "text":""
-            }
-        ], 
-        "friday":[
-            {
-                "title":"",
-                "text":""
-            },
-            {
-                "title":"",
-                "text":""
+                "title":"Bibliographic consultation",
+                "text":"I continued my research for information and photographs in the library. The following documents have been consulted: <br> - L. Brandina, S. Cicchetti (edited by), <i>La notte della Riviera '90</i>, Rimini, Maggioli, 1990. <br> - A. La Motta, <i>Cartoline per Rimini</i>, Rimini, Raffaelli, 2000. <br> - Rimini Turismo, <i>Rimini: Benvenuti a Rimini capitale dell'ospitalità</i>, Rimini, Rimini Turismo, 1997. <br> - C. Cederna, <i>Casa nostra</i>, Milano, A. Mondadori, 1983. <br> - A. Bonomi, <i>Il distretto del piacere</i>, Torino, Bollati Boringhieri, 2000. <br> - F. Farina, <i>Una costa lunga due secoli: storie e immagini della riviera di Rimini</i>, Rimini, Fondazione Cassa di Risparmio, 2003. <br> - F. Farina, <i>1843-1993: centocinquant'anni di vita balneare</i>, Rimini, Societa Gas, 1993. <br> - O. Maroni (edited by), <i>Davide Minghini fotografo in Rimini: immagini dall'archivio</i>, Bologna, Compositori, 2003."
             }
         ],
+        "friday":[
+            {
+                "title":"Bibliographic consultation",
+                "text":" The following documents have been consulted: <br> - V. Bessone, <i>La divina notte: viaggio quasi dantesco fra i gironi delle discoteche riminesi</i>, Rimini, Guaraldi, 1994. <br> - P. Pacoda, <i>Riviera Club Culture: la scena dance nella metropoli balneare d'Europa</i>, Cerasolo Ausa di Coriano, NdA Press, 2012. <br> - M. Zardini, <i>Nomare: nascita e sviluppo della metropoliriviera</i>, Bologna, Editrice Compositori, 2006. <br> - C. Branzaglia (edited by), <i>NightWave: atti dei convegni</i>, Genova, Costa & Nolan, 1998. <br> - P. V. Tondelli, <i>Un weekend postmoderno: cronache dagli anni Ottanta</i>, Milano, Bompiani, 1990."
+            },
+
+        ], 
     },
 
     {
